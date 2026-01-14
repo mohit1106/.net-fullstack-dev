c5280fec0656ad4b9ee794d9f0b7285a033ac2e4
Mohit Verma <mohit51341862@gmail.com>
2026-01-14 10:10:02 +0530

    multi-threading

---
+++ 16-threading-async-programming/Program.cs
@@ -0,0 +1,11 @@
+﻿Using System.Threading;
+
+class Program {
+    static void Main() {
+        Thread thread = new Thread( new ParameterizedThreadStart(PrintMessage));
+        thread.Start("heelo from thread");
+    }
+    static void PrintMessage(object message) {
+        Console.WriteLine(message);
+    }
+}
