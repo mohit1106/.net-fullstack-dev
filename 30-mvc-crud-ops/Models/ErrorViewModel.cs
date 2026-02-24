ca0f0fcb36a7a15c1af98f4f56818f0434ac8fba
Mohit Verma <mohit51341862@gmail.com>
2026-02-24 11:32:48 +0530

    adonet

---
+++ 30-mvc-crud-ops/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _30_mvc_crud_ops.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
