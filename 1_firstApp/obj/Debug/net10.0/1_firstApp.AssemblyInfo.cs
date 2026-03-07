5711356d678fb473e185c1ecb38eec4515f4fd43
Mohit Verma <mohit51341862@gmail.com>
2026-03-07 17:55:33 +0530

    pagination

--- 1_firstApp/obj/Debug/net10.0/1_firstApp.AssemblyInfo.cs
+++ 1_firstApp/obj/Debug/net10.0/1_firstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1_firstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b350b889a64849928e294d852348c8520ea9f9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6d5c25e00a1f8baec386ac987bc932789788a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("1_firstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1_firstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
